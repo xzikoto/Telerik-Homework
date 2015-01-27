c75b7cb2284e2b18588667b41cf38ab2428c229a
Ivailo G <ivailogramatikov05@gmail.com>
2015-01-27 16:55:45 +0200

    Commit 1

---
+++ Introduction/Square-Root/Square-Root/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Square_Root
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 12345;
+            Console.WriteLine(Math.Sqrt(number));
+
+        }
+    }
+}
